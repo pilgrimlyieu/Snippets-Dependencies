f9e636fc9ea122bc23f60a3eec92d6a520afad1c
PilgrimLyieu <PilgrimLyieu@outlook.com>
2022-05-28 13:19:24 +0800

    Update `markdown_cmdsub.py`

--- markdown_cmdsub.py
+++ markdown_cmdsub.py
@@ -8,18 +8,18 @@ def command_cycle(target, commands, bracketnum = 1):
     for i in range(length):
         target = target.replace(commands[i], command_map[i])
     string = list(target)
-    depth = brackets = 0
+    depth = braces = 0
     for i in range(len(string) - 1, -1, -1):
         if string[i] == '}': depth += 1
         elif string[i] == '{': depth -= 1
-        elif brackets == bracketnum:
+        elif braces == bracketnum:
             if not depth:
                 try:
                     string[i] = choose_next(string[i], command_map, length)
                     break
                 except:
                     pass
-        brackets += 0 if depth else 1
+        braces += 0 if depth else 1
     result = ''.join(string)
     for i in range(length):
         result = result.replace(command_map[i], commands[i])
@@ -27,14 +27,14 @@ def command_cycle(target, commands, bracketnum = 1):
 
 def command_swap(target, command_1, command_2, bracketnum = 1):
     string = list(target.replace(command_1, '🚀').replace(command_2, '🚁'))
-    depth = brackets = 0
+    depth = braces = 0
     for i in range(len(string) - 1, -1, -1):
         if string[i] == '}': depth += 1
         elif string[i] == '{': depth -= 1
-        elif brackets == bracketnum:
+        elif braces == bracketnum:
             if not depth:
                 if string[i] == '🚀': string[i] = '🚁'
                 elif string[i] == '🚁': string[i] = '🚀'
             break
-        brackets += 0 if depth else 1
+        braces += 0 if depth else 1
     return ''.join(string).replace('🚀', command_1).replace('🚁', command_2)
