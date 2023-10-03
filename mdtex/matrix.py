8c2667c1a30f2c4eb992e3bdad1f851b3a1260bb
PilgrimLyieu <PilgrimLyieu@outlook.com>
2023-10-03 19:24:13 +0800

    Add common case & Fix some issues

--- mdtex/matrix.py
+++ mdtex/matrix.py
@@ -25,10 +25,10 @@ def debug(matrix, snip, display = 1):
 def rcn_replace(content, row, col, row_num):
     return content.replace('\\r', str(row + 1)).replace('\\c', str(col + 1)).replace('\\n', str(row * row_num + col + 1))
 def dynvar_replace(content, coordinate, row_num):
-    return re.sub(r'`([\s\d+*/-]*)`', lambda m: str(eval(m.group(1))), rcn_replace(content, *coordinate, row_num))
-def add_tabstop(tabstop, coordinate, placeholders):
+    return re.sub(r'`([\s\d+*/-]*)`', lambda m: str(eval(m.group(1))), rcn_replace(content, *coordinate, row_num)).replace("}", "\\}")
+def add_tabstop(tabstop, coordinate, placeholders, default = ""):
     content = placeholders[tabstop][2]
-    return "${%d:%s" % (tabstop, content) + "}" if (coordinate == placeholders[tabstop][0:2] and content) else "$%d" % tabstop
+    return "${%d:%s" % (tabstop, content) + "}" if (coordinate == placeholders[tabstop][0:2] and content) else "$%d" % tabstop if tabstop else default
 row_vec = lambda matrix,  row: matrix[row - 1]
 col_vec = lambda matrix,  col: [row[col - 1] for row in matrix]
 
@@ -50,18 +50,24 @@ def generate_matrix(form, options, size, content, snip):
     tabstop_for_0_sep = 1 if '2' in options else 0
     tabstop_in_style  = 0 if '3' in options else 1
 
-
     empty_element = "0" if fill_zero else ""
 
-    if   len(size) == 0: row_num,  col_num = default_row_num, default_col_num
-    elif len(size) == 1: row_num = col_num = int(size)
+    if   len(size) == 1: row_num = col_num = int(size)
     elif len(size) == 2: row_num,  col_num = int(size[0]), int(size[1])
+    else:                row_num,  col_num = default_row_num, default_col_num
     matrix = [[empty_element for j in range(col_num)] for i in range(row_num)]
 
     placeholders = [(-1, -1, '')]
     tabstop_matrix = [[0 for j in range(col_num)] for i in range(row_num)]
     tabstop = 1
-    if matrix_style == '0':
+    if matrix_style == '':
+        for i, j in product(range(row_num), range(col_num)):
+            if tabstop_in_style:
+                tabstop_matrix[i][j] = tabstop
+                placeholders.append((i, j, dynvar_replace(content, (i, j), col_num)))
+                tabstop += 1
+            matrix[i][j] = add_tabstop(tabstop_matrix[i][j], (i, j), placeholders, dynvar_replace(content, (i, j), col_num))
+    elif matrix_style == '0':
         if tabstop_for_0_sep:
             for i, j in product(range(row_num), range(col_num)):
                 tabstop_matrix[i][j] = tabstop
@@ -73,7 +79,6 @@ def generate_matrix(form, options, size, content, snip):
             for i, j in product(range(row_num), range(col_num)):
                 tabstop_matrix[i][j] = tabstop
                 matrix[i][j] = add_tabstop(tabstop, (i, j), placeholders)
-        debug(matrix, snip, display)
     elif row_num == col_num:
         match matrix_style:
             case 'i':
@@ -88,18 +93,17 @@ def generate_matrix(form, options, size, content, snip):
                         matrix[i][i] = dynvar_replace(content, (0, 0), col_num)
                 if tabstop_for_0_sep:
                     for i, j in product(range(row_num), range(col_num)):
-                        if i != j:
+                        if matrix[i][j] == empty_element:
                             tabstop_matrix[i][j] = tabstop
                             placeholders.append((i, j, empty_element))
                             tabstop += 1
-                        matrix[i][j] = add_tabstop(tabstop_matrix[i][j], (i, j), placeholders)
+                            matrix[i][j] = add_tabstop(tabstop_matrix[i][j], (i, j), placeholders)
                 elif tabstop_for_0_all:
-                    placeholders.append((0, 0, empty_element))
+                    placeholders.append((0, 1, empty_element))
                     for i, j in product(range(row_num), range(col_num)):
-                        if i != j:
+                        if matrix[i][j] == empty_element:
                             tabstop_matrix[i][j] = tabstop
-                        matrix[i][j] = add_tabstop(tabstop_matrix[i][j], (i, j), placeholders)
-                debug(matrix, snip, display)
+                            matrix[i][j] = add_tabstop(tabstop_matrix[i][j], (i, j), placeholders)
             case 'd':
                 pass
             case 'D':
@@ -112,7 +116,6 @@ def generate_matrix(form, options, size, content, snip):
                         tabstop, matrix[i][j] = add_tabstop(tabstop, matrix[i][j])
                     elif tabstop_for_0_all:
                         _, matrix[i][j] = add_tabstop(row_num if tabstop_in_style else 0, matrix[i][j])
-                debug(matrix, snip, display)
             case 't':
                 for i, j in product(range(row_num), range(col_num)):
                     if i <= j:
@@ -121,7 +124,6 @@ def generate_matrix(form, options, size, content, snip):
                             tabstop, matrix[i][j] = add_tabstop(tabstop, matrix[i][j])
                     elif tabstop_for_0_sep:
                         tabstop, matrix[i][j] = add_tabstop(tabstop, matrix[i][j])
-                debug(matrix, snip, display)
             case 'T':
                 for i, j in product(range(row_num), range(col_num)):
                     if i >= j:
@@ -130,16 +132,16 @@ def generate_matrix(form, options, size, content, snip):
                             tabstop, matrix[i][j] = add_tabstop(tabstop, matrix[i][j])
                     elif tabstop_for_0_sep:
                         tabstop, matrix[i][j] = add_tabstop(tabstop, matrix[i][j])
-                debug(matrix, snip, display)
             case 'c':
                 for i in range(row_num):
                     pass
+    debug(matrix, snip, display)
 
 if __name__ == "__main__":
     # print(matrix_template("b", [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]], True))
     # print(matrix_template("b", [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]], False))
-    generate_matrix("b", "i1", "4", "a", None)
-    generate_matrix("b", "i2", "4", "a", None)
+    generate_matrix("b", "i13", "4", "a_{\\r\\c}", None)
+    generate_matrix("b", "i3", "4", "a", None)
 
 # Adapted from https://github.com/sillybun/zyt-snippet
 def generate_matrix_element(i, j, row, column, virtual_row, virtual_column, ht, vt):
