c7b952b1706cb3046ba5e4282d5e130843de7083
PilgrimLyieu <PilgrimLyieu@outlook.com>
2024-10-10 09:39:20 +0800

    Update `mdtex/scopes.py` to make it extensible

--- mdtex/scopes.py
+++ mdtex/scopes.py
@@ -4,11 +4,25 @@ external_environments = ['sympy', 'wolfram']
 
 # 数学模式 Math Mode
 def math():
-    return vim.eval('vimtex#syntax#in_mathzone()') == '1' and not extcal()
+    except_conditions = [
+        extcal(),
+        # roman()
+    ]
+    return all([
+        vim.eval('vimtex#syntax#in_mathzone()') == '1',
+        not any(except_conditions)
+    ])
 
 # 纯数学模式 Pure Math Mode
 def pure_math():
-    return math() and not_chem() and not_unit()
+    extra_conditions = [
+        not_chem(),
+        not_unit()
+    ]
+    return all([
+        math(),
+        all(extra_conditions)
+    ])
 
 def extcal():
     for environment in external_environments:
@@ -17,38 +31,42 @@ def extcal():
     return False
 
 # 行内公式模式 Inline Math Mode
-def inline_math():
+def inline_math() -> bool:
     return vim.eval("vimtex#syntax#in('texMathZone[LT]I')") == '1'
 
 # 行间公式模式 Display Math Mode
-def display_math():
+def display_math() -> bool:
     return vim.eval("vimtex#syntax#in('texMathZone[LT]D')") == '1'
 
 # 化学模式 Chemistry Mode
-def chem():
+def chem() -> bool:
     return vim.eval("get(vimtex#cmd#get_current(), 'name')") == '\\ce' and math()
 
 # 非化学模式 Not Chemistry Mode
-def not_chem():
+def not_chem() -> bool:
     return vim.eval("get(vimtex#cmd#get_current(), 'name')") != '\\ce'
 
 # 单位模式 Unit Mode
-def unit():
+def unit() -> bool:
     return vim.eval("get(vimtex#cmd#get_current(), 'name')") == '\\pu' and math()
 
 # 非单位模式 Not Unit Mode
-def not_unit():
+def not_unit() -> bool:
     return vim.eval("get(vimtex#cmd#get_current(), 'name')") != '\\pu'
 
+# 罗马模式 Roman Mode
+def roman() -> bool:
+    return vim.eval("get(vimtex#cmd#get_current(), 'name')") == '\\mathrm'
+
 # 文本模式 Text Mode
-def text():
+def text() -> bool:
     return vim.eval('vimtex#syntax#in_mathzone()') == '0'
 
 # 注释模式 Comment Mode
-def comment(): 
+def comment() -> bool: 
     return vim.eval('vimtex#syntax#in_comment()') == '1'
 
 # 特殊环境 Specific Environment
-def env(name):
+def env(name) -> bool:
     [x, y] = vim.eval("vimtex#env#is_inside('" + name + "')") 
     return x != '0' and y != '0'
